a982372fd92388effe677faf35d30af1f5a9e6ff
bcorie <bealecorie@gmail.com>
2022-12-21 21:16:53 -0500

    Add project files.

---
+++ Box Stacking Simulation/Box.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Corie Beale
+ * IGME 206
+ * E9
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Box_Stacking_Simulation
+{
+    internal class Box
+    {
+        private Random rand = new Random();
+
+        /// <summary>
+        /// Create an empty box.
+        /// </summary>
+        public Box()
+        {
+            Items = 0;
+        }
+
+        /// <summary>
+        /// Create an empty box with a random number of items, inclusive.
+        /// </summary>
+        /// <param name="minItems">minimum items</param>
+        /// <param name="maxItems">maximum items</param>
+        public Box(int minItems, int maxItems)
+        {
+            Items = rand.Next(minItems, maxItems + 1);
+        }
+
+        /// <summary>
+        /// Items in the box.
+        /// </summary>
+        public int Items
+        {
+            get;
+        }
+    }
+}
---
+++ Box Stacking Simulation/Program.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Corie Beale
+ * IGME 206
+ * E9
+ */
+
+
+namespace Box_Stacking_Simulation
+{
+    class Program
+    {
+        const int DAYS = 3;
+        const int STACKS = 4;
+        const int MINHEIGHT = 1;
+        const int MAXHEIGHT = 3;
+        const int MINITEMS = 1;
+        const int MAXITEMS = 9;
+
+        static void Main(string[] args)
+        {
+            Box[,,] array = createArray();
+            printArray(array);
+        }
+
+        /// <summary>
+        /// Creates an array using given constants
+        /// </summary>
+        /// <returns>the array created</returns>
+        static Box[,,] createArray()
+        {
+            // array[ROW,COLUMN,ARRAY]
+            Box[,,] array = new Box[MAXHEIGHT,STACKS,DAYS];
+            Random rand = new Random();
+
+            // depending on stack #, find height and populate
+            int height;
+            Box temp;
+            for (int k = 0; k < DAYS; k++) {
+                for (int j = 0; j < STACKS; j++)
+                {
+                    height = rand.Next(MINHEIGHT - 1, MAXHEIGHT + 1);
+                    for (int i = 0; i < MAXHEIGHT; i++)
+                    {
+                        if (i <= height)
+                        {
+                            temp = new Box(MINITEMS, MAXITEMS);
+                            array[i,j,k] = temp;
+                        }
+                        else
+                        {
+                            array[i,j,k] = new Box();
+                        }
+                    }
+                }
+            }
+            return array;
+        }
+
+        /// <summary>
+        /// Prints the array provided.
+        /// </summary>
+        /// <param name="a">array</param>
+        static void printArray(Box[,,] a)
+        {
+            for (int k = 0; k < DAYS; k++)
+            {
+                Console.WriteLine("\nDay " + (k + 1) + ":");
+
+                for (int i = MAXHEIGHT - 1; i >= 0; i--)
+                {
+                    for (int j = 0; j < STACKS; j++)
+                    {
+                        if (a[i,j,k].Items == 0)
+                        {
+                            Console.Write(" ");
+                        }
+                        else
+                        {
+                            Console.Write(a[i, j, k].Items);
+                        }
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
